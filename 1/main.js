e8394f4ac70cce87895973dc43baf9c30a824bad
Xondamir0 <alimjanovxondamir01@gmail.com>
2024-12-27 00:23:32 +0500

    Add files via upload

---
+++ 1/main.js
@@ -0,0 +1,36 @@
+
+
+//var username = "sardor";
+//var username = "safina";
+
+
+// console.log(username);
+//console.log(ism);
+
+//let number = 18;
+//let number2 = 2;
+//console.log("Javob",number + number2);
+
+// let username = prompt("Enter your name");
+// let surname = prompt("Enter your surname");
+// let userage = prompt("Enter your age");
+//console.log(number);
+
+
+// const number1 = r
+// const number3 =()
+// alert(siz kiritgan raqam bu:${number1} + ${number3} = ${number1 + number3} );
+
+const num = prompt("Enter your name?")
+const num1 = +prompt("Enter your age?")
+const num2 = prompt("Enter your surname")
+const num3 = prompt("Enter your favourite subject")  
+
+
+
+
+
+console.log(`${num} ${typeof num} name
+    ${num1} ${typeof num1} age
+    ${num2} ${typeof num2} surname
+    ${num3} ${typeof num3} subject`);
